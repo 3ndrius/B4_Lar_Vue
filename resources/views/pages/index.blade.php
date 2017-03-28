1c244743e6fa6ad6cd2d6f6ef0a98a2ad22659ba
3ndrius <andrzejo1991@gmail.com>
2017-03-29 01:18:33 +0200

    Fix b4 size

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,13 +4,13 @@
 
 @section('content')
 
+<div class="container">
 
-<div class="container-fluid">
+
+
   <div class="row">
-<div class="col-md-1">
 
-</div>
-<div class="col-md-10 col-md-offset-1">
+<div class="col-md-12">
 
 
   <div class="jumbotron jumbotron-fluid">
@@ -21,14 +21,12 @@
   </div>
 </div>
 </div>
-</div>
 
-<div class="container-fluid">
+
+
   <div class="row">
-  <div class="col-md-1">
 
-  </div>
-    <div class="col-md-8">
+    <div class="col-md-10">
 
       <div class="card-columns">
         @foreach($posts as $post)
@@ -45,17 +43,15 @@
 </div>
 
 
-    <div class="col-md-2 col-md-offset-1">
+    <div class="col-md-2 ">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
     </div>
   </div>
 
   <div class="row">
-    <div class="col-md-1">
-    </div>
 
-    <div class="col-md-10 col-md-offset-1">
+
       <div id="accordion" role="tablist" aria-multiselectable="true">
 
 
@@ -105,10 +101,11 @@
   </div>
 </div
 
-    </div>
+
   </div>
 
 
+</div>
 </div>
 
 
