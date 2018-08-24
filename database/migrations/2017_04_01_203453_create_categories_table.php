cd0189ffdee3d4f9346cff850b8ebc8fd7d58521
3ndrius <3ndrius@users.noreply.github.com>
2018-08-24 10:23:44 +0200

    Update 2017_04_01_203453_create_categories_table.php

--- database/migrations/2017_04_01_203453_create_categories_table.php
+++ database/migrations/2017_04_01_203453_create_categories_table.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -19,7 +18,6 @@ class CreateCategoriesTable extends Migration
             $table->timestamps();
         });
     }
-
     /**
      * Reverse the migrations.
      *
